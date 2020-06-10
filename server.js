f7bf8e02b33129d1ec902977aeaf13d418660df9
MaciejSpalek <spwrtt@gmail.com>
2020-06-10 16:53:01 +0200

    feat: add basic listening on connect and disconnect users

--- server.js
+++ server.js
@@ -6,4 +6,18 @@ const cors = require('cors');
 
 app.use(cors());
 
+io.on('connection', socket => {
+    console.log("user has already logged in")
+    const online = Object.keys(io.engine.clients);
+    io.emit('users', JSON.stringify(online));
+    
+    socket.on('disconnect', ()=>{
+        console.log("user has already logged out");
+        const online = Object.keys(io.engine.clients);
+        io.emit('users', JSON.stringify(online));
+    });
+
+});
+
+
 http.listen(5000, () => console.log('Server started'))
